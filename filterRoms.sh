6cb91ab1a27cff6f1dc9c029b94bac233299f8d1
Benjamin Dumont <benjamindumont@MacBook-Pro-INEAT-BDumont.local>
2017-02-10 16:58:59 +0100

    Adding filter for 7zip

--- filterRoms.sh
+++ filterRoms.sh
@@ -7,18 +7,14 @@ function fctInit {
   tarMimeType="application/tar"
   targzMimeType="application/tar+gzip"
   gzipMimeType="application/x-gzip"
-}
+  sevenZMimeType="application/x-7z-compressed"
+  scriptPath=$(dirname $0)
+  archivePath="${scriptPath}/input"
+  destinationPath="${scriptPath}/output"
 
-function fctCheckArgs {
-  if [ -z "$archivePath" ] || [ -z $destinationPath  ]; then
-    fctErrorArguments
-  fi
-  if [ ! -f "$archivePath" ]; then
-    fctErrorNotAFile $archivePath
-  fi
-  if [ ! -d "$destinationPath" ]; then
-   mkdir -p "$destinationPath" 
-  fi
+  echo $scriptPath
+  echo $archivePath
+  echo $destinationPath
 }
 
 function fctGetPatterns {
@@ -28,7 +24,7 @@ function fctGetPatterns {
     if [ $? -eq 0 ]; then
       patterns+=($line)
     fi  
-  done < patterns.txt
+  done < ${scriptPath}/patterns.txt
   
   if [ ${#patterns[@]} -eq 0 ]; then
     fctErrorPattern
@@ -38,13 +34,65 @@ function fctGetPatterns {
   echo ${patterns[1]}
 }
 
-function fctUncompress {
-  mimetype=`file --mime-type  ${archivePath} | cut -d ':' -f 2`
+function fctCheckEnv {
+  inputData=`find ${archivePath} -type f | grep -v .gitignore | wc -l | tr -d ' '`
+  outputData=`find ${destinationPath} -type f | grep -v .gitignore | wc -l | tr -d ' '`
+  if [ $inputData -eq 0 ]; then
+    fctErrorDirectoryEmpty "${archivePath}"
+  fi
+  if [ $outputData -ne 0 ]; then
+    fctErrorDirectoryNotEmpty "${destinationPath}"
+  fi
+}
+
+function fctProceed {
+
+find ${archivePath} -type f -print0  | while IFS= read -r -d '' archiveFile; do 
+  echo  "$archiveFile" | grep -v '.gitignore' 2>/dev/null
+  if [ $? -eq 0 ]; then
+    echo "${archiveFile} will be processed"
+    fctIdentifyMimeType "$archiveFile"
+  fi 
+done
+}
+
+
+
+function fctIdentifyMimeType {
+  mimetype=`file --mime-type  "$1" | cut -d ':' -f 2`
   echo "Mime-Type found: $mimetype"
 
   if [ $mimetype = $rarMimeType ] || [ $mimetype = $rarMimeType2 ]; then
     echo "File is a rar file"
-  fi
+  elif [ $mimetype = ${sevenZMimeType} ]; then
+    echo "File is a 7z file"
+    fct7zProcess "$1"
+  fi 
+}
+
+function fct7zProcess {
+while read -r line
+do
+    filename="$line"
+    for pattern in "${patterns[@]}"
+    do
+	filename=`echo $filename | grep ${pattern}`
+    done
+    if [ "$filename" != "" ]; then
+      echo 7z x "$1" -o "$destinationPath" "$filename"
+      7z e \"$1\" \"$filename\" 
+    fi
+done < <(7z l "$1" | awk -F ' ' '{ $1 = ""; $2 = ""; $3 = ""; $4 = ""; print $0}')
+}
+
+function fctErrorDirectoryEmpty {
+  echo "$1 should not be empty"
+  exit 1
+}
+
+function fctErrorDirectoryNotEmpty {
+  echo "$1 should be empty"
+  exit 1
 }
 
 function fctErrorPattern {
@@ -62,13 +110,7 @@ function fctErrorNotAFile {
   exit 1
 }
 
-function fctErrorArguments {
-  echo "Usage: ./filterRoms.sh <archive path> <destination path>"
-  exit 1
-}
-fctGetPatterns
-archivePath=$1
-destinationPath=$2
 fctInit
-fctCheckArgs
-fctUncompress
+fctGetPatterns
+fctCheckEnv
+fctProceed
