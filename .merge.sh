7974b4aaaa6cbb12aa294eb87c97e6684f4890a8
benjdum59 <benjdum59@gmail.com>
2017-03-16 16:12:52 +0100

    test

--- .merge.sh
+++ .merge.sh
@@ -5,10 +5,13 @@ echo "AFTER SUCCESS SCRIPT"
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
   echo "Works $TRAVIS_BRANCH"
   echo "$TRAVIS_REPO_SLUG"
-  url="https://${GITHUB_KEY}@github.com:${TRAVIS_REPO_SLUG}.git"
+  url="https://${GITHUB_KEY}@github.com/${TRAVIS_REPO_SLUG}.git"
   echo $url
   cd /tmp
   git clone ${url}
+  directory=`echo ${TRAVIS_REPO_SLUG} | awk -F '/' ' { print $2  } '`
+  echo $directory
+  cd $directory
   git checkout master
   git merge develop -m "Merge from develop into master after build success"
   git push origin master
