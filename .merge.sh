e84c5ac415d4c2e966e83cf724a30e9299d93e5b
benjdum59 <benjdum59@gmail.com>
2017-03-16 16:24:25 +0100

    test

--- .merge.sh
+++ .merge.sh
@@ -6,11 +6,11 @@ if [ "$TRAVIS_BRANCH" == "develop" ]; then
   echo "Works $TRAVIS_BRANCH"
   echo "$TRAVIS_REPO_SLUG"
   url="https://${GITHUB_KEY}@github.com/${TRAVIS_REPO_SLUG}.git"
-  echo $url
   cd /tmp
   git clone ${url}
+  export GIT_COMMITTER_EMAIL='travis@travis'
+  export GIT_COMMITTER_NAME='Travis CI'
   directory=`echo ${TRAVIS_REPO_SLUG} | awk -F '/' ' { print $2  } '`
-  echo $directory
   cd $directory
   git checkout master
   git merge origin/develop --no-edit
