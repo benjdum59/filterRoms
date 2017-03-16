d0595c8ff8bba119d139d2fb8f70d0b223fbaaf2
benjdum59 <benjdum59@gmail.com>
2017-03-16 16:01:08 +0100

    test

--- .merge.sh
+++ .merge.sh
@@ -5,4 +5,6 @@ echo "AFTER SUCCESS SCRIPT"
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
   echo "Works $TRAVIS_BRANCH"
   echo "$TRAVIS_REPO_SLUG"
+  url=${GITHUB_KEY}@github.com:${TRAVIS_REPO_SLUG}.git
+  echo $url
 fi
